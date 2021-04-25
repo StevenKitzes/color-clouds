33536236f60e751ae46954e589b4b530fdf631ac
Steven Kitzes <steven.kitzes@gmail.com>
2021-04-24 18:02:11 -0700

    feat: seeding bloom, bloom contagion, box averaging

--- main.js
+++ main.js
@@ -16,10 +16,11 @@ const coinToss = () => {
   return randInt(0, 1) ? true : false
 }
 
-function Box(red, green, blue, spanId) {
+function Box(red, green, blue, bloom, spanId) {
   this.red = red
   this.green = green
   this.blue = blue
+  this.bloom = bloom
   this.spanId = spanId
 }
 
@@ -36,12 +37,13 @@ const initBoxes = (boxSize, displayElement, boxData, boxDataStage) => {
     boxDataStage.push([])
     for (let col = 0; col < width; col++) {
       const span = document.createElement('span')
-      const red = randInt(0, 255)
-      const green = randInt(0, 255)
-      const blue = randInt(0, 255)
+      const red = 0
+      const green = 0
+      const blue = 0
+      const bloom = 0
       const spanId = `box-${displayElement.id}-${row}-${col}`
-      boxData[row].push(new Box(red, green, blue, spanId))
-      boxDataStage[row].push(new Box(red, green, blue, spanId))
+      boxData[row].push(new Box(red, green, blue, bloom, spanId))
+      boxDataStage[row].push(new Box(red, green, blue, bloom, spanId))
 
       span.id = spanId
       span.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
@@ -69,7 +71,7 @@ const colorDrift = (factor) => {
 }
 
 // returns ref to its interval to be canceled if a new loop is started
-const startLoop = (boxData, boxDataStage, colorDriftFactor, frameDelay) => {
+const startLoop = (bloomFactor, bloomInitial, bloomOdds, bloomSpreadOdds, boxData, boxDataStage, colorDriftFactor, frameDelay) => {
   return setInterval(() => {
     // general strategy: READ from boxData to WRITE to boxDatastage
     // fuss around and customize values in boxDataStage, maintaining frame-wise original,
@@ -78,21 +80,142 @@ const startLoop = (boxData, boxDataStage, colorDriftFactor, frameDelay) => {
 
     // for each box:
     // if i have bloom:
-    //   noisy average me with self and other bloom neighbors ONLY for those neighbors with higher bloom count than mine
+    //   average me with self and other bloom neighbors ONLY for those neighbors with higher bloom count than mine
     //   reduce my remaining bloom count
     // if i don't have bloom:
-    //   noisy average me with all neighbors
+    //   average me with all neighbors
     //   weight the average toward boxes with bloom
-    //   if i have multiple bloom neighbors, select one randomly
-    //     chance of catching some amount of bloom and becoming a bloom box
+    //   chance of catching some amount of bloom and becoming a bloom box
 
-    // step: fuss with stage data
+    // step: populate stage data from boxData
     boxData.forEach((row, r) => {
       row.forEach((currentBox, c) => {
-        boxDataStage[r][c].red = currentBox.red + colorDrift(colorDriftFactor)
-        boxDataStage[r][c].green = currentBox.green + colorDrift(colorDriftFactor)
-        boxDataStage[r][c].blue = currentBox.blue + colorDrift(colorDriftFactor)
+        boxDataStage[r][c].red = currentBox.red
+        boxDataStage[r][c].green = currentBox.green
+        boxDataStage[r][c].blue = currentBox.blue
       })
+    })
+
+    // step: seed new blooms
+    if (randInt(0, bloomOdds) === 0) {
+      log('bloom!')
+      const x = randInt(0, boxDataStage[0].length - 1)
+      const y = randInt(0, boxDataStage.length - 1)
+      boxDataStage[y][x].red = randInt(0, 255)
+      boxDataStage[y][x].green = randInt(0, 255)
+      boxDataStage[y][x].blue = randInt(0, 255)
+      boxDataStage[y][x].bloom = bloomInitial
+    }
+    
+    // step: apply bloom/averaging rules
+    boxDataStage.forEach((row, r) => {
+      row.forEach((box, c) => {
+        // If current has bloom, avg w self and other higher bloom neightbors and reduce bloom
+        if (box.bloom > 0) {
+          document.getElementById(box.spanId).innerHTML = 'B'
+          const avg = {
+            red: box.red,
+            green: box.green,
+            blue: box.blue,
+            count: 1
+          }
+          if (c > 0 && boxData[r][c-1].bloom >= box.bloom) {
+            const other = boxData[r][c-1]
+            avg.red += other.red
+            avg.green += other.green
+            avg.blue += other.blue
+            avg.count++
+          }
+          if (c < row.length - 1 && boxData[r][c+1].bloom >= box.bloom) {
+            const other = boxData[r][c+1]
+            avg.red += other.red
+            avg.green += other.green
+            avg.blue += other.blue
+            avg.count++
+          }
+          if (r > 0 && boxData[r-1][c].bloom >= box.bloom) {
+            const other = boxData[r-1][c]
+            avg.red += other.red
+            avg.green += other.green
+            avg.blue += other.blue
+            avg.count++
+          }
+          if (r < boxDataStage.length - 1 && boxData[r+1][c].bloom >= box.bloom) {
+            const other = boxData[r+1][c]
+            avg.red += other.red
+            avg.green += other.green
+            avg.blue += other.blue
+            avg.count++
+          }
+          box.red = avg.red / avg.count
+          box.green = avg.green / avg.count
+          box.blue = avg.blue / avg.count
+          box.bloom--
+        }
+        // If box lacks bloom, avg w all neighbors; weight bloom neighbors; maybe catch their bloom
+        else if (box.bloom < 1) {
+          document.getElementById(box.spanId).innerHTML = ''
+          const avg = {
+            red: box.red,
+            green: box.green,
+            blue: box.blue,
+            count: 1
+          }
+          let maxNeighborBloom = 0
+          if (c > 0) {
+            const other = boxData[r][c-1]
+            avg.red += other.red * (other.bloom > 0 ? bloomFactor : 1)
+            avg.green += other.green * (other.bloom > 0 ? bloomFactor : 1)
+            avg.blue += other.blue * (other.bloom > 0 ? bloomFactor : 1)
+            avg.count += (other.bloom > 0 ? bloomFactor : 1)
+            maxNeighborBloom = Math.max(maxNeighborBloom, other.bloom)
+          }
+          if (c < row.length - 1) {
+            const other = boxData[r][c+1]
+            avg.red += other.red * (other.bloom > 0 ? bloomFactor : 1)
+            avg.green += other.green * (other.bloom > 0 ? bloomFactor : 1)
+            avg.blue += other.blue * (other.bloom > 0 ? bloomFactor : 1)
+            avg.count += (other.bloom > 0 ? bloomFactor : 1)
+            maxNeighborBloom = Math.max(maxNeighborBloom, other.bloom)
+          }
+          if (r > 0) {
+            const other = boxData[r-1][c]
+            avg.red += other.red * (other.bloom > 0 ? bloomFactor : 1)
+            avg.green += other.green * (other.bloom > 0 ? bloomFactor : 1)
+            avg.blue += other.blue * (other.bloom > 0 ? bloomFactor : 1)
+            avg.count += (other.bloom > 0 ? bloomFactor : 1)
+            maxNeighborBloom = Math.max(maxNeighborBloom, other.bloom)
+          }
+          if (r < boxDataStage.length - 1) {
+            const other = boxData[r+1][c]
+            avg.red += other.red * (other.bloom > 0 ? bloomFactor : 1)
+            avg.green += other.green * (other.bloom > 0 ? bloomFactor : 1)
+            avg.blue += other.blue * (other.bloom > 0 ? bloomFactor : 1)
+            avg.count += (other.bloom > 0 ? bloomFactor : 1)
+            maxNeighborBloom = Math.max(maxNeighborBloom, other.bloom)
+          }
+          box.red = avg.red / avg.count
+          box.green = avg.green / avg.count
+          box.blue = avg.blue / avg.count
+          if (randInt(0, bloomSpreadOdds) === 0) {
+            box.bloom = Math.max(maxNeighborBloom - 1, 0)
+          }
+        }
+      })
+    })
+    
+    // step: apply drift
+    boxDataStage.forEach(row => {
+      row.forEach(box => {
+        box.red += colorDrift(colorDriftFactor)
+        box.green += colorDrift(colorDriftFactor)
+        box.blue += colorDrift(colorDriftFactor)
+      })
+    })
+
+    // step: enforce bounds on stage before writing to boxData and DOM
+    boxDataStage.forEach(row => {
+      row.forEach(box => enforceColorBoundsOnBox(box))
     })
 
     // step: write fussed stage to DOM and boxData
@@ -103,6 +226,7 @@ const startLoop = (boxData, boxDataStage, colorDriftFactor, frameDelay) => {
         boxData[r][c].red = currentBox.red
         boxData[r][c].green = currentBox.green
         boxData[r][c].blue = currentBox.blue
+        boxData[r][c].bloom = currentBox.bloom
         })
     })
   }, frameDelay)
@@ -110,6 +234,10 @@ const startLoop = (boxData, boxDataStage, colorDriftFactor, frameDelay) => {
 
 // returns ref to its interval for cancelation in case of a new loop start
 const colorClouds = (args = {}) => {
+  const bloomFactor = args.bloomFactor || 3
+  const bloomInitial = args.bloomInitial || 100
+  const bloomOdds = args.bloomOdds || 100
+  const bloomSpreadOdds = args.bloomSpreadOdds || 30
   const boxSize = args.boxSize || 20
   const colorDriftFactor = args.colorDriftFactor || 5
   const elementId = args.elementId || 'color-clouds'
@@ -136,5 +264,5 @@ const colorClouds = (args = {}) => {
   initBoxes(boxSize, displayElement, boxData, boxDataStage)
 
   // return ref to the loop interval from setInterval
-  return startLoop(boxData, boxDataStage, colorDriftFactor, frameDelay)
+  return startLoop(bloomFactor, bloomInitial, bloomOdds, bloomSpreadOdds, boxData, boxDataStage, colorDriftFactor, frameDelay)
 }
